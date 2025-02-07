05adf4c03fadece304f465dccdbe76eeb76b793f
Topepepepe <letusbanana@gmail.com>
2025-02-08 03:28:22 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+// script.js
+
+// Function to handle button click events
+function selectOption(option) {
+    if (option === 'yes') {
+        flashRainbowColors(function() {
+            document.getElementById('question').style.display = 'none';
+            displayCatHeart();
+        });
+    } else if (option === 'no') {
+        document.getElementById('no-button').innerText = 'You sure?';
+        var yesButton = document.getElementById('yes-button');
+        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        var newSize = parseFloat(currentFontSize) * 2;
+        yesButton.style.fontSize = newSize + 'px';
+    } else {
+        alert('Invalid option!');
+    }
+}
+
+// Function to flash rainbow colors and then execute a callback function
+function flashRainbowColors(callback) {
+    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var i = 0;
+    var interval = setInterval(function() {
+        document.body.style.backgroundColor = colors[i];
+        i = (i + 1) % colors.length;
+    }, 200);
+    setTimeout(function() {
+        clearInterval(interval);
+        document.body.style.backgroundColor = '#800000'; // Reset background color to maroon
+        if (callback) {
+            callback();
+        }
+    }, 2000);
+}
+
+// Function to display the cat.gif initially
+function displayCat() {
+    var imageContainer = document.getElementById('image-container');
+    var catImage = new Image();
+    catImage.src = 'cat.gif';
+    catImage.alt = 'Cat';
+    catImage.onload = function() {
+        imageContainer.appendChild(catImage);
+    };
+}
+
+// Function to display the cat-heart.gif
+function displayCatHeart() {
+    document.getElementById('image-container').innerHTML = '';
+    var imageContainer = document.getElementById('image-container');
+    var catHeartImage = new Image();
+    catHeartImage.src = 'cat-heart.gif';
+    catHeartImage.alt = 'Cat Heart';
+    catHeartImage.onload = function() {
+        imageContainer.appendChild(catHeartImage);
+        document.getElementById('options').style.display = 'none';
+    };
+}
+
+// Set the initial background color to maroon
+document.body.style.backgroundColor = '#800000';
+
+displayCat();
