e85e9fb4a24edc053bb015ffdfc9f6a1bf446271
Topepepepe <letusbanana@gmail.com>
2025-02-08 03:33:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function selectOption(option) {
 
 // Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var colors = ['#800000'];
     var i = 0;
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
